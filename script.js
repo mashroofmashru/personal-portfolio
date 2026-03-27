ab66a3fb52be3365b2ac96ccef797adfa94f687b
mashroofmashru <mashroofvlk@gmail.com>
2026-03-27 11:18:30 +0530

    feat:redesing entire website layout

---
+++ script.js
@@ -0,0 +1,5 @@
+const obs = new IntersectionObserver((entries) => {
+    entries.forEach(e => { if (e.isIntersecting) e.target.classList.add('visible'); });
+}, { threshold: 0.08 });
+
+document.querySelectorAll('.fade-up, .edu-item, .project-card, .mini-card').forEach(el => obs.observe(el));
